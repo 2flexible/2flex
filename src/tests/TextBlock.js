b359bd3a5d46a1e50813f3f32612566a1bb95286
nazarov <nezerovabuzar@gmail.com>
2025-07-14 13:01:09 +0400

    test folder removed

--- src/tests/TextBlock.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TextBlock = void 0;
-class TextBlock {
-    constructor() { }
-}
-exports.TextBlock = TextBlock;
--- src/tests/shapes/Rect.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Shape_1 = require("../Shape");
-class Rect extends Shape_1.Shape {
-}
-exports.default = Rect;
