0c6262d6f218acd7e7acd9f91edb878c490cd4bb
nazarov <nezerovabuzar@gmail.com>
2025-11-18 12:24:56 +0400

    overral refactor on Line

--- src/shapes/Line.ts
+++ src/shapes/Line.ts
@@ -1,194 +1,219 @@
 import {
     Shape,
-    LineCapOpt,
     QuadraticCurveToOpt,
     BezierCurveToOpt,
+    LineCapOpt,
+    LineJoinOpt,
+    LineDashOffset,
+    Fill,
 } from "../Shape";
 import { IBlock } from "../types";
 import { Path } from "../Path";
 
-export type LineJoinOpt = "miter" | "round" | "bevel";
-
-interface LineOptions extends BezierCurveToOpt, QuadraticCurveToOpt {
-    points: number[];
-    lineDashOffset?: number;
-    lineJoin: LineJoinOpt;
+interface Points {
+    startX1?: number;
+    startY1?: number;
+    startX2?: number;
+    startY2?: number;
+    endX?: number;
+    endY?: number;
+    closePath?: boolean;
 }
-type Point = BezierCurveToOpt;
+interface LineOptions extends BezierCurveToOpt, QuadraticCurveToOpt {
+    points: Points[];
+    dragStartX: boolean;
+    dragStartY: boolean;
+    dragStartX1: boolean;
+    dragStartX2: boolean;
+    dragStartY1: boolean;
+    dragStartY2: boolean;
+    dragEndX: boolean;
+    dragEndY: boolean;
+}
 
 export class Line extends Shape {
-    joinTo: Line | undefined = undefined;
-    path: Path;
-    #beforeX: number;
-    #beforeY: number;
+    path?: Path;
+    #startCords = { x: 0, y: 0 };
 
     constructor(options: IBlock<LineOptions>) {
         super(options);
         this.options = options;
-        this.#beforeX = this.x();
-        this.#beforeY = this.y();
         this.path = new Path();
+        this.#startCords = { x: this.x(), y: this.y() };
     }
-    __initSet(): void {
-        super.__initSet();
-        // this.#beforeEndX = this.options.x;
-        // this.#beforeEndY = this.options.y;
-    }
-    __drawInit() {
-        if (!this.joinTo) {
-            this.beginPath();
-            this.path.createPath();
-            this.path.path.moveTo(this.canvasInit.x, this.canvasInit.y);
-            this.strokeStyle();
-        }
-        if (!this.options.points) {
-            const { cpx1, cpy1, cpx2, cpy2, endX, endY } = this.#calcDiff(
-                this.options.cpx1,
-                this.options.cpy1,
-                this.options.cpx2,
-                this.options.cpy2,
-                this.options.endX,
-                this.options.endY
-            );
-            this.options.cpx1 = cpx1;
-            this.options.cpy1 = cpy1;
-            this.options.cpx2 = cpx2;
-            this.options.cpy2 = cpy2;
-            this.options.endX = endX;
-            this.options.endY = endY;
-            if (
-                this.options.cpx1 &&
-                this.options.cpy1 &&
-                !this.options.cpx2 &&
-                !this.options.cpy2
-            ) {
-                this.path!.path.quadraticCurveTo(
-                    this.options.cpx1,
-                    this.options.cpy1,
-                    this.options.endX,
-                    this.options.endY
-                );
-            } else if (
-                this.options.cpx1 &&
-                this.options.cpy1 &&
-                this.options.cpx2 &&
-                this.options.cpy2
-            ) {
-                this.path!.path.bezierCurveTo(
-                    this.options.cpx1,
-                    this.options.cpy1,
-                    this.options.cpx2,
-                    this.options.cpy2,
-                    this.options.endX,
-                    this.options.endY
+
+    draw(_func?: (context: this) => void): void {
+        this.beginPath();
+        this.path?.createPath();
+        this.path?.path.moveTo(this.#startCords.x, this.#startCords.y);
+
+        const points = this.points();
+        if (!points) {
+            let startX1 = this.startX1();
+            let startY1 = this.startY1();
+            let startX2 = this.startX2();
+            let startY2 = this.startY2();
+            let endX = this.endX();
+            let endY = this.endY();
+
+            if (startX1 && this.dragStartX1()) startX1 += this.#startCords.x;
+            if (startX2 && this.dragStartX2()) startX2 += this.#startCords.x;
+            if (startY1 && this.dragStartY1()) startY1 += this.#startCords.y;
+            if (startY2 && this.dragStartY2()) startY2 += this.#startCords.y;
+            if (endX && this.dragEndX()) endX += this.#startCords.x;
+            if (endY && this.dragEndY()) endY += this.#startCords.y;
+
+            if (startX1 && startY1 && !startX2 && !startY2) {
+                this.path?.path.quadraticCurveTo(startX1, startY1, endX, endY);
+            } else if (startX1 && startY1 && startX2 && startY2) {
+                this.path?.path.bezierCurveTo(
+                    startX1,
+                    startY1,
+                    startX2,
+                    startY2,
+                    endX,
+                    endY
                 );
             } else {
-                this.path.lineTo(this.options.endX, this.options.endY);
+                this.path?.lineTo(endX, endY);
             }
         } else {
-            this.#drawLines();
+            for (let idx = 0; idx < points.length; idx++) {
+                const point = points[idx];
+
+                let startX1 = point.startX1;
+                let startY1 = point.startY1;
+                let startX2 = point.startX2;
+                let startY2 = point.startY2;
+                let endX = point.endX;
+                let endY = point.endY;
+
+                if (startX1 !== undefined) startX1 += this.canvasInit.x;
+                if (startY1 !== undefined) startY1 += this.canvasInit.y;
+                if (startX2 !== undefined) startX2 += this.canvasInit.x;
+                if (startY2 !== undefined) startY2 += this.canvasInit.y;
+                if (endX !== undefined) endX += this.canvasInit.x;
+                if (endY !== undefined) endY += this.canvasInit.y;
+
+                if (
+                    startX1 !== undefined &&
+                    startY1 !== undefined &&
+                    startX2 === undefined &&
+                    startY2 === undefined
+                ) {
+                    this.path?.path.quadraticCurveTo(
+                        startX1,
+                        startY1,
+                        endX!,
+                        endY!
+                    );
+                } else if (
+                    startX1 !== undefined &&
+                    startY1 !== undefined &&
+                    startX2 !== undefined &&
+                    startY2 !== undefined
+                ) {
+                    this.path?.path.bezierCurveTo(
+                        startX1,
+                        startY1,
+                        startX2,
+                        startY2,
+                        endX!,
+                        endY!
+                    );
+                } else {
+                    this.path?.path.lineTo(endX!, endY!);
+                }
+                if (point.closePath) this.path!.path.closePath();
+            }
         }
-
-        this.fill();
-        this.stroke(this.options.stroke, this.path!.path);
+        if (this.closePath()) this.path!.path.closePath();
+        if (this.fill()) this.context.fill(this.path!.path);
+        if (this.stroke()) this.context.stroke(this.path!.path);
     }
 
-    dash(opt?: number[]) {
-        this.options.dash = super.lineDash(opt);
-        return this.options.dash;
+    fill(opt?: Fill): boolean {
+        return this.__cacheOption(opt, "fill", false);
     }
-    points(opt?: object[]) {
-        this.options.points = opt || this.options.points || [];
-        return this.options.points;
+    stroke(opt?: Fill): boolean {
+        return this.__cacheOption(opt, "stroke", false);
+    }
+
+    points(opt?: Points[]) {
+        return this.__cacheOption(opt, "points", undefined);
     }
     checkInBound(_event: MouseEvent): boolean {
         const { x, y } = this.canvas.getCursorPosition(_event);
         return this.pointInStroke({ path: this.path!.path, x: x, y: y });
     }
-    #calcDiff(
-        cpx1: number,
-        cpy1: number,
-        cpx2: number,
-        cpy2: number,
-        endX: number,
-        endY: number
-    ) {
-        const diffX = this.x() - this.#beforeX;
-        if (diffX !== 0) {
-            endX += diffX;
-            if (cpx1) cpx1 += diffX;
-            if (cpx2) cpx2 += diffX;
-
-            this.#beforeX = this.x();
-        }
-        const diffY = this.y() - this.#beforeY;
-        if (diffY !== 0) {
-            endY += diffY;
-            if (cpy1) cpy1 += diffY;
-            if (cpy2) cpy2 += diffY;
-
-            this.#beforeY = this.y();
-        }
-        return { cpx1, cpy1, cpx2, cpy2, endX, endY };
+    startX1(opt?: number) {
+        return this.__cacheOption(opt, "startX1", 0);
     }
-    #drawLines() {
-        const beforeX = this.#beforeX;
-        const beforeY = this.#beforeY;
-        this.options.points.forEach((point: Point, index: number, arr: []) => {
-            const { cpx1, cpy1, cpx2, cpy2, endX, endY } = this.#calcDiff(
-                point.cpx1,
-                point.cpy1,
-                point.cpx2,
-                point.cpy2,
-                point.endX,
-                point.endY
-            );
-            this.options.points[index] = { cpx1, cpy1, cpx2, cpy2, endX, endY };
-            if (
-                cpx1 !== undefined &&
-                cpy1 !== undefined &&
-                cpx2 === undefined &&
-                cpy2 === undefined
-            ) {
-                this.path!.path.quadraticCurveTo(cpx1, cpy1, endX, endY);
-            } else if (
-                cpx1 !== undefined &&
-                cpy1 !== undefined &&
-                cpx2 !== undefined &&
-                cpy2 !== undefined
-            ) {
-                this.path!.path.bezierCurveTo(
-                    cpx1,
-                    cpy1,
-                    cpx2,
-                    cpy2,
-                    endX,
-                    endY
-                );
-            } else {
-                this.path!.path.lineTo(endX, endY);
-            }
-            if (arr.length - 1 !== index) {
-                this.#beforeX = beforeX;
-                this.#beforeY = beforeY;
-            }
-        });
+    startY1(opt?: number) {
+        return this.__cacheOption(opt, "startY1", 0);
     }
-    joinBorder(opt?: LineCapOpt) {
+    startX2(opt?: number) {
+        return this.__cacheOption(opt, "startX2", 0);
+    }
+    startY2(opt?: number) {
+        return this.__cacheOption(opt, "startY2", 0);
+    }
+    endX(opt?: number) {
+        return this.__cacheOption(opt, "endX", 0);
+    }
+    endY(opt?: number) {
+        return this.__cacheOption(opt, "endY", 0);
+    }
+    dragStartX(opt?: boolean) {
+        return this.__cacheOption(opt, "dragStartX", true);
+    }
+    dragStartY(opt?: boolean) {
+        return this.__cacheOption(opt, "dragStartY", true);
+    }
+    dragStartX1(opt?: boolean) {
+        return this.__cacheOption(opt, "dragStartX1", true);
+    }
+    dragStartY1(opt?: boolean) {
+        return this.__cacheOption(opt, "dragStartY1", true);
+    }
+    dragStartX2(opt?: boolean) {
+        return this.__cacheOption(opt, "dragStartX2", true);
+    }
+    dragStartY2(opt?: boolean) {
+        return this.__cacheOption(opt, "dragStartY2", true);
+    }
+    dragEndX(opt?: boolean) {
+        return this.__cacheOption(opt, "dragEndX", true);
+    }
+    dragEndY(opt?: boolean) {
+        return this.__cacheOption(opt, "dragEndY", true);
+    }
+    fillStyle(opt?: string): string {
+        return super.fillStyle(opt);
+    }
+    lineWidth(opt?: number): number {
+        const lineWidth = this.__cacheOption(opt, "lineWidth", 0);
+        if (this.canvasInit.width === 0) this.canvasInit.width = lineWidth;
+        this.context.lineWidth = this.canvasInit.width;
+        return lineWidth;
+    }
+    lineCap(opt?: LineCapOpt): LineCapOpt {
         return super.lineCap(opt);
     }
-
-    join(line: Line) {
-        line.joinTo = this;
-        line.path = this.path;
+    lineJoin(opt?: LineJoinOpt): LineJoinOpt {
+        return super.lineJoin(opt);
     }
-    strokeWidth(opt?: number) {
-        const strokeWidth = this.__cacheOption(opt, "strokeWidth", 1);
-        this.options.strokeWidth = super.lineWidth(strokeWidth);
-        return this.options.strokeWidth;
+    lineDash(opt?: number[]) {
+        return super.lineDash(opt);
     }
-    fillStyle(opt?: string) {
-        return super.fillStyle(opt);
+    lineDashOffset(opt?: LineDashOffset) {
+        return super.lineDashOffset(opt);
+    }
+    strokeStyle(opt?: string): string {
+        return super.strokeStyle(opt);
+    }
+    closePath(opt?: boolean): boolean {
+        return this.__cacheOption(opt, "closePath", false);
     }
     clip(opt?: boolean): boolean {
         return super.clip(opt);
@@ -200,13 +225,83 @@ export class Line extends Shape {
         return super.dragY(opt);
     }
     draggable(opt: boolean): boolean {
-        return super.draggable(opt);
-    }
+        const draggable = this.__cacheOption(opt, "draggable", true);
+        if (!draggable) return false;
+        if (!this.selectable()) return false;
 
+        let isMouseDown = false;
+
+        let initX = 0;
+        let initY = 0;
+
+        let beforeX = 0;
+        let beforeY = 0;
+
+        this.mousedown((event) => {
+            const { x, y } = this.canvas.getCursorPosition(event);
+            initX = x;
+            initY = y;
+            if (event.button === 0) {
+                isMouseDown = true;
+                beforeX = 0;
+                beforeY = 0;
+            }
+        });
+
+        this.mousemove((event) => {
+            if (isMouseDown) {
+                const { x, y } = this.canvas.getCursorPosition(event);
+                let diffX = x - initX;
+                let diffY = y - initY;
+                const startX1 = this.startX1();
+                const startX2 = this.startX2();
+                const endX = this.endX();
+                const startY1 = this.startY1();
+                const startY2 = this.startY2();
+                const endY = this.endY();
+
+                // const dragX1 =
+                //     this.dragStartX1() && startX1 - 20 < x && x < startX1;
+                // const dragX2 =
+                //     this.dragStartX2() && startX2 - 20 < x && x < startX2;
+
+                // const dragEndX =
+                //     this.dragEndX() &&
+                //     endX + 20 > x &&
+                //     x > endX + this.#startCords.x;
+
+                // const dragEndY =
+                //     this.dragEndY() &&
+                //     endY + 20 > y &&
+                //     y > endY + this.#startCords.y;
+
+                this.beforeCords.x = this.canvasInit.x;
+                if (diffX !== 0 && this.dragX()) {
+                    if (this.dragStartX()) this.canvasInit.x += diffX - beforeX;
+                    // if (dragX1 || dragX2 || dragEndX)
+                    this.#startCords.x += diffX - beforeX;
+                    beforeX = diffX;
+                }
+                this.beforeCords.y = this.canvasInit.y;
+                if (diffY !== 0 && this.dragY()) {
+                    if (this.dragStartY()) this.canvasInit.y += diffY - beforeY;
+                    // if (dragEndY)
+                    this.#startCords.y += diffY - beforeY;
+                    beforeY = diffY;
+                }
+                this.__adjustCordinates();
+                this.canvas.invokeChange?.call(this.canvas);
+            }
+        });
+
+        this.mouseup((event) => {
+            isMouseDown = false;
+        });
+        return draggable;
+    }
     selectable(opt?: boolean): boolean {
         return super.selectable(opt);
     }
-
     set(options: IBlock<LineOptions>) {
         super.set(options);
     }
