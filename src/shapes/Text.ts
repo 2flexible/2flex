6f55f1e1b04c5fed2a13324766b0a152ad447de0
nazarov <nezerovabuzar@gmail.com>
2026-01-18 22:08:12 +0400

    some of the repated methodds removed from text shape

--- src/shapes/Text.ts
+++ src/shapes/Text.ts
@@ -65,17 +65,6 @@ export class Text extends Shape<IText> {
         }
     }
 
-    resizable(opt?: boolean): boolean {
-        return super.resizable(opt);
-    }
-
-    selectable(opt?: boolean): boolean {
-        return super.selectable(opt);
-    }
-
-    maxWidth(opt?: number | string): number {
-        return super.maxWidth(opt);
-    }
     width(opt?: number): number {
         const text_measure = super.measureText(this.text);
         return super.width(opt || text_measure?.width);
@@ -102,98 +91,17 @@ export class Text extends Shape<IText> {
     fontVariant(opt?: FontVariant) {
         return this.__valueHandler(opt, "fontVariant", "normal");
     }
-
     fontStyle(opt?: FontStyle) {
         return this.__valueHandler(opt, "fontStyle", "normal");
     }
-
-    fontStretch(opt?: FontStretch) {
-        return super.fontStretch(opt);
-    }
-
-    fontKerning(opt?: FontKerning) {
-        return super.fontKerning(opt);
-    }
-
-    fontVariantCaps(opt?: FontVariantCaps) {
-        return super.fontVariantCaps(opt);
-    }
-
-    wordSpacing(opt?: string) {
-        return super.wordSpacing(opt);
-    }
-
     color(opt?: string) {
         const color = this.__valueHandler(opt, "color", "black");
         super.fillStyle(color);
         return color;
     }
-
-    strokeWidth(opt?: number) {
-        return super.lineWidth(opt);
-    }
-
-    strokeColor(opt?: string) {
-        return super.strokeStyle(opt);
-    }
-
-    direction(opt?: TextDirection) {
-        return super.direction(opt);
-    }
-
-    letterSpacing(opt?: string) {
-        return super.letterSpacing(opt);
-    }
-
-    textRendering(opt?: TextRendering) {
-        return super.textRendering(opt);
-    }
     // need to change for parent element instead of usign context align
     textAlign(opt?: TextAlign) {
         const textAlign = this.__valueHandler(opt, "textAlign", "start");
         return textAlign;
-    }
-    textBaseline(opt?: TextBaseline) {
-        return super.textBaseline(opt);
-    }
-    blur(opt?: number) {
-        return super.blur(opt);
-    }
-    brightness(opt?: number) {
-        return super.brightness(opt);
-    }
-    contrast(opt?: number) {
-        return super.contrast(opt);
-    }
-    dropShadow(opt?: [number, number, number, string][]) {
-        return super.dropShadow(opt);
-    }
-    grayscale(opt?: number) {
-        return super.grayscale(opt);
-    }
-    hueRotate(opt?: number) {
-        return super.hueRotate(opt);
-    }
-    opacity(opt?: number) {
-        return super.opacity(opt);
-    }
-    sepia(opt?: number) {
-        return super.sepia(opt);
-    }
-    hidden(opt?: boolean): boolean {
-        return super.hidden(opt);
-    }
-    dragX(opt?: boolean) {
-        return super.dragX(opt);
-    }
-    dragY(opt?: boolean) {
-        return super.dragY(opt);
-    }
-    draggable(opt: boolean): boolean {
-        return super.draggable(opt);
-    }
-
-    set(options: IBlock<IText>) {
-        super.set(options);
     }
 }
