17766bfbe23d0ef06d85a7206fc1bec7bef71714
nazarov <nezerovabuzar@gmail.com>
2026-02-21 15:20:20 +0400

    responsiveness created for text

--- src/shapes/Text.ts
+++ src/shapes/Text.ts
@@ -35,46 +35,98 @@ export interface ITextOptions {
 }
 
 export class Text extends Shape<ITextOptions> {
-    text: string;
+    #splitedText: string[] = [];
+    #lettersWidth: number[] = [];
+    #updateText?: () => void;
+
     constructor(text: string, options: IBlock<ITextOptions>) {
         super(options);
-        this.text = text;
-        this.options.text = text;
+        this.text(text);
     }
 
     draw(_func?: (context: CanvasRenderingContext2D) => void): void {
+        const cacheR = this.rotate();
+        this.rotate(0);
         super.font(this.#format_font);
-        if (this.fill()) {
-            super.fillText({
-                text: this.text,
-                x: this.x(),
-                y: this.height() + this.y(),
-                maxWidth: this.width(),
-            });
+        const text_measure = super.measureText(this.text());
+        this.#updateText?.();
+        this.#updateText = undefined;
+        let words = [this.text()];
+        if (text_measure.width > this.getRealWidth)
+            words = this.#responsiveText(text_measure.width);
+        let wrapH = this.getTop.y;
+        for (let i = 0, len = words.length; i < len; i++) {
+            wrapH += text_measure.hangingBaseline;
+            if (this.fill()) {
+                super.fillText({
+                    text: words[i],
+                    x: this.getLeft.x,
+                    y: wrapH,
+                    maxWidth: text_measure.width,
+                });
+            }
+
+            if (super.stroke()) {
+                super.strokeText({
+                    text: words[i],
+                    x: this.getLeft.x,
+                    y: wrapH,
+                    maxWidth: text_measure.width,
+                });
+            }
         }
-
-        if (super.stroke()) {
-            super.strokeText({
-                text: this.text,
-                x: this.x(),
-                y: this.height() + this.y(),
-                maxWidth: this.width(),
-            });
-        }
-    }
-
-    width(opt?: number): number {
-        const text_measure = super.measureText(this.text);
-        return super.width(opt || text_measure?.width);
-    }
-
-    height(opt?: number): number {
-        const text_measure = super.measureText(this.text);
-        return super.height(opt || text_measure?.hangingBaseline);
+        this.rotate(cacheR);
     }
 
     get #format_font() {
         return `${this.fontStyle()} ${this.fontVariant()} ${this.fontWeight()} ${this.fontSize()}px ${this.fontFamily()}`;
+    }
+
+    #responsiveText(width: number) {
+        let realWidth = width;
+        const texts = [];
+        let wordIdx = 0;
+        let words = "";
+        let additonalSplit: string[] = [];
+        let lenWords = 0;
+        while (
+            realWidth > this.getRealWidth &&
+            wordIdx < this.#splitedText.length
+        ) {
+            additonalSplit = [];
+            lenWords += this.#lettersWidth[wordIdx];
+            if (lenWords >= this.getRealWidth) {
+                texts.push(words);
+                words = "";
+                lenWords = this.#lettersWidth[wordIdx];
+                additonalSplit = this.#splitedText.slice(
+                    wordIdx,
+                    this.#splitedText.length
+                );
+                realWidth = super.measureText(additonalSplit.join(" ")).width;
+            }
+            words += this.#splitedText[wordIdx] + " ";
+            wordIdx += 1;
+        }
+        if (additonalSplit.length !== 0) texts.push(additonalSplit.join(" "));
+        return texts;
+    }
+
+    text(opt?: string): string {
+        const cacheT = this.ownOptions.text || "";
+        const text = this.__valueHandler(opt, "text", "");
+        if (text.length !== cacheT.length) {
+            this.#updateText = () => {
+                this.#splitedText = text.split(" ");
+                const spaceW = super.measureText(" ").width;
+                for (let i = 0, len = this.#splitedText.length; i < len; i++) {
+                    this.#lettersWidth.push(
+                        super.measureText(this.#splitedText[i]).width + spaceW
+                    );
+                }
+            };
+        }
+        return text;
     }
 
     fontFamily(opt?: string) {
@@ -112,5 +164,10 @@ export class Text extends Shape<ITextOptions> {
         const width = this.__valueHandler(opt, "border", 0);
         super.lineWidth(width);
         return width;
+    }
+
+    scale(opt?: number): void {
+        super.scale(opt);
+        this.fontSize(this.fontSize() * (opt || 1));
     }
 }
