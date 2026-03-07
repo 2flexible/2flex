126a94f87dd3f1f0771d44df38d30db61dfb40da
nazarov <nezerovabuzar@gmail.com>
2026-03-07 19:26:32 +0400

    selecting all when dblclick in textBlock, and some fixes

--- src/shapes/TextBlock.ts
+++ src/shapes/TextBlock.ts
@@ -69,6 +69,7 @@ export class TextBlock extends ShapeBlock<ITextOptions> {
     #stopTraverseSign = 0;
     #editable = false;
     #caretDrawer?: () => void;
+    #highlightDrawer?: () => void;
 
     constructor(text: string, options: IBlock<ITextOptions>) {
         super(options);
@@ -93,6 +94,9 @@ export class TextBlock extends ShapeBlock<ITextOptions> {
 
         let heightP = 0;
         let heights = 0;
+
+        if (this.#editable) this.#highlightDrawer?.();
+
         for (let i = 0, len = words.length; i < len; i++) {
             if (i !== 0) heightP = sumOfHeights;
             if (this.fill()) {
@@ -118,8 +122,12 @@ export class TextBlock extends ShapeBlock<ITextOptions> {
         this.rotate(cacheR);
     }
     __hotLines(): void {
-        if (!this.#editable) super.__hotLines();
-        else this.#caretDrawer?.();
+        if (!this.#editable) {
+            super.__hotLines();
+            this.#caretDrawer = undefined;
+        } else {
+            this.#caretDrawer?.();
+        }
     }
 
     get #format_font() {
@@ -170,10 +178,6 @@ export class TextBlock extends ShapeBlock<ITextOptions> {
         if (!editable) return;
         const beforeValues: any = {};
 
-        this.dblclick((e) => {
-            this.#editable = true;
-        });
-
         let foundNode: LetterNode | undefined;
         let foundNodeId: number | undefined;
         const dummyLetter: LetterNode = {
@@ -187,9 +191,28 @@ export class TextBlock extends ShapeBlock<ITextOptions> {
             x: 0,
             y: 0,
         };
-        const click = (event: MouseEvent) => {
+
+        let dbClick = false;
+
+        this.dblclick(() => {
+            this.#editable = true;
+            dbClick = true;
+            foundNode = undefined;
+            foundNodeId = undefined;
+            this.#caretDrawer = undefined;
+            this.#drawHighlight(
+                this.x(),
+                this.y(),
+                this.width(),
+                this.height()
+            );
+        });
+
+        const mousedown = (event: MouseEvent) => {
             if (!this.checkInBound(event)) this.#editable = false;
             if (!this.#editable) return;
+            this.#highlightDrawer = undefined;
+            dbClick = false;
             const initCords = this.canvas?.getCursorPosition(event);
             this.#traverseLetterNodes((node) => {
                 if (
@@ -221,8 +244,26 @@ export class TextBlock extends ShapeBlock<ITextOptions> {
             }
         };
         this.keydown((e: KeyboardEvent) => {
-            if (!foundNode || foundNodeId === undefined) return;
+            if (!this.#editable) return;
             beforeValues[this.nodeId!] = {};
+            if (dbClick) {
+                foundNode = {
+                    nodeId: 0,
+                    prev: undefined,
+                    next: undefined,
+                    letter: "",
+                    width: 0,
+                    wordWidth: 0,
+                    height: 0,
+                    x: 0,
+                    y: 0,
+                };
+                this.#letterNode = foundNode;
+                foundNodeId = 0;
+                this.#highlightDrawer = undefined;
+                dbClick = false;
+            }
+            if (!foundNode || foundNodeId === undefined) return;
             if (e.key === "Backspace") {
                 if (foundNodeId > 0) {
                     this.#removeLetterNode(foundNode);
@@ -231,15 +272,13 @@ export class TextBlock extends ShapeBlock<ITextOptions> {
             } else if (e.key === "Tab") {
                 dummyLetter.letter = "    ";
                 this.#addAfter(foundNode, dummyLetter);
-                if (foundNodeId !== undefined) foundNodeId += 4;
+                foundNodeId += 4;
             } else {
                 dummyLetter.letter = e.key;
                 this.#addAfter(foundNode, dummyLetter);
-                if (foundNodeId !== undefined) foundNodeId += 1;
+                foundNodeId += 1;
             }
-            if (foundNodeId !== undefined) {
-                foundNode = this.#findNode(foundNodeId);
-            }
+            foundNode = this.#findNode(foundNodeId);
             if (foundNode) {
                 this.#drawCaret(
                     foundNode.x + foundNode.width,
@@ -251,7 +290,7 @@ export class TextBlock extends ShapeBlock<ITextOptions> {
             dummyLetter.letter = "";
         });
 
-        this.__eventHandler("click", click, "editableClick");
+        this.__eventHandler("mousedown", mousedown, "editableClick");
     }
 
     text(opt?: string): string {
@@ -264,6 +303,8 @@ export class TextBlock extends ShapeBlock<ITextOptions> {
                 let prevNode = this.#letterNode;
                 let pendingNode = undefined;
                 let wordWidth = 0;
+                const measure = super.measureText("");
+                this.#letterNode.height = measure.hangingBaseline;
                 for (let i = 0, len = splitedText.length; i < len; i++) {
                     const measure = super.measureText(splitedText[i]);
                     const node = {
@@ -281,7 +322,7 @@ export class TextBlock extends ShapeBlock<ITextOptions> {
                     prevNode = prevNode.next;
                     if (!pendingNode) pendingNode = prevNode;
                     wordWidth += measure.width;
-                    if (splitedText[i] === " ") {
+                    if (splitedText[i] === " " || i === len - 1) {
                         pendingNode.wordWidth = wordWidth;
                         pendingNode = undefined;
                         wordWidth = 0;
@@ -313,6 +354,7 @@ export class TextBlock extends ShapeBlock<ITextOptions> {
             letters += node.letter;
         });
         this.text(letters);
+        this.canvas.invokeChange();
     }
 
     #addBefore(targetNode: LetterNode, newNode: LetterNode) {
@@ -353,6 +395,7 @@ export class TextBlock extends ShapeBlock<ITextOptions> {
             prevNode = node;
         });
         this.text(letters);
+        this.canvas.invokeChange();
     }
 
     #drawCaret(x: number, y: number, width: number, height: number) {
@@ -364,6 +407,16 @@ export class TextBlock extends ShapeBlock<ITextOptions> {
             this.context.strokeStyle = "red";
             this.context.lineWidth = 2;
             this.context.stroke();
+        };
+        this.canvas?.invokeChange();
+    }
+
+    #drawHighlight(x: number, y: number, width: number, height: number) {
+        if (!this.context) return;
+        this.#highlightDrawer = () => {
+            this.context.beginPath();
+            this.context.fillStyle = "rgba(0, 13, 255, 0.47)";
+            this.context.fillRect(x, y, width, height);
         };
         this.canvas?.invokeChange();
     }
