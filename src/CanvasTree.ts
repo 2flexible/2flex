8022a68a5cb7922eb9ab87a2e3b0dd50cf48981f
nazarov <nezerovabuzar@gmail.com>
2026-03-15 14:18:14 +0400

    Tree renamed as CanvasTree, Node seperated from Tree

---
+++ src/CanvasTree.ts
@@ -0,0 +1,100 @@
+import { SnapshotObject, Timestamp } from "./types";
+import { Node } from "./Node";
+
+export interface Snapshots {
+    [key: number]: SnapshotObject;
+}
+
+export class CanvasTree {
+    head: Node;
+    #currentSnapshot;
+    #snapshots: Snapshots;
+    snapshotSize: number;
+    #latestNodeId;
+    nodes: Node[];
+
+    constructor(snapshotSize?: number) {
+        this.head = new Node();
+        this.snapshotSize = snapshotSize || 0;
+        this.#currentSnapshot = 0;
+        this.#snapshots = {};
+        this.#latestNodeId = 1;
+        this.nodes = [];
+    }
+
+    addNodes(node: Node[]) {
+        this.head.addChild(...node);
+    }
+
+    preOrderTraversal<T>(_func?: (node: T) => void) {
+        this.nodes = [];
+        this.head.listAllChilds((current: Node) => {
+            this.assignNodeId(current);
+            if (_func) _func(current as T);
+            this.nodes.push(current);
+        });
+    }
+
+    assignNodeId(node: Node) {
+        if (node.nodeId === undefined) {
+            node.nodeId = this.#latestNodeId;
+            this.#latestNodeId += 1;
+        }
+    }
+
+    takeSanpshot(
+        timestamp: Timestamp,
+        before: SnapshotObject | null,
+        after: SnapshotObject
+    ) {
+        for (const key of Object.keys(this.#snapshots)) {
+            if (Number(key) > this.#currentSnapshot)
+                delete this.#snapshots[Number(key)];
+        }
+        for (const key of Object.keys(this.#snapshots)) {
+            if (this.snapshotSize < Object.entries(this.#snapshots).length) {
+                delete this.#snapshots[Number(key)];
+            } else break;
+        }
+        if (this.#snapshots[timestamp])
+            this.#snapshots[timestamp] = {
+                ...this.#snapshots[timestamp],
+                ...after,
+            };
+        else this.#snapshots[timestamp] = after;
+
+        this.#currentSnapshot = timestamp;
+        if (before)
+            for (const key of Object.keys(this.#snapshots).reverse()) {
+                if (Number(key) < this.#currentSnapshot) {
+                    for (let [kk, value] of Object.entries(before)) {
+                        this.#snapshots[Number(key)][kk] = {
+                            ...this.#snapshots[Number(key)][kk],
+                            ...value,
+                        };
+                    }
+                    break;
+                }
+            }
+    }
+
+    snapshotInBack() {
+        for (const key of Object.keys(this.#snapshots).reverse()) {
+            if (Number(key) < this.#currentSnapshot) {
+                this.#currentSnapshot = Number(key);
+                break;
+            }
+        }
+        return this.#snapshots[this.#currentSnapshot];
+    }
+
+    snapshotInFuture() {
+        for (const key of Object.keys(this.#snapshots)) {
+            if (Number(key) > this.#currentSnapshot) {
+                this.#currentSnapshot = Number(key);
+                break;
+            }
+        }
+        return this.#snapshots[this.#currentSnapshot];
+    }
+}
